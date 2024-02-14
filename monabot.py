6cefdf762ddc93eead967cb28ae6463077b90e5b
simon <simon.janssen@umontreal.ca>
2024-02-14 17:21:40 -0500

    Ajoute limite au nombre de modifs et corrige la source

--- monabot.py
+++ monabot.py
@@ -8,18 +8,25 @@ with open('wikidata_ids.json', 'r') as file_contents:
 
     site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
+
+    limit = 10
+    counter = 0
     
     for artist_id in artist_ids:
-        item = pywikibot.ItemPage(repo, artist_id)
-        
-        print("Adding P136 claim to "+artist_id)
-        if 'P136' not in item.claims:
-            claim = pywikibot.Claim(repo, 'P136') #Adding artistic genre of oeuvre (P136)
-            target = pywikibot.ItemPage(repo, 'Q557141') #Connecting P136 with 'Public art' (Q557141)
-            claim.setTarget(target)
-            item.addClaim(claim, summary="Bot: Adding claim Public Art as artist's genre to "+artist_id)
-
-            source = "https://picasso.iro.umontreal.ca/~mona/api/v3/artists"
-            claim.addSource(source, summary='Bot: Adding sources to artistic genre of oeuvre (P136).')
+        if counter < limit:
+            item = pywikibot.ItemPage(repo, artist_id)
+            
+            print("Adding P136 claim to "+artist_id)
+            if 'P136' not in item.claims:
+                claim = pywikibot.Claim(repo, 'P136') #Adding artistic genre of oeuvre (P136)
+                target = pywikibot.ItemPage(repo, 'Q557141') #Connecting P136 with 'Public art' (Q557141)
+                claim.setTarget(target)
+                item.addClaim(claim, summary="Bot: Adding claim Public Art as artist's genre to "+artist_id+".")
+    
+                url = pywikibot.Claim(repo, 'P854') #P854, reference URL
+                url.setTarget("https://picasso.iro.umontreal.ca/~mona/api/v3/artists")
+                claim.addSource(url, summary='Bot: Adding sources to artistic genre of oeuvre (P136).')
+            else:
+                print(" "+artist_id+" already has claim for P136\n")
         else:
-            print(" "+artist_id+" already has claim for P136\n")
+            print("Did not modify "+artist_id+"; hit modification limit.")
