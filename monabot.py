d18b9fd3cb5003f77b9266cca0ab163c3a22ed7b
simon <simon.janssen@umontreal.ca>
2024-03-01 07:11:45 -0500

    Improves checks for artists which may already have public art as genre of oeuvre

--- monabot.py
+++ monabot.py
@@ -6,6 +6,7 @@ with open('wikidata_ids.json', 'r') as file_contents:
 
     artist_ids = json.load(file_contents)
 
+    #site = pywikibot.Site("test", "wikidata")
     site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
 
@@ -14,11 +15,15 @@ with open('wikidata_ids.json', 'r') as file_contents:
     
     for artist_id in artist_ids:
         if counter < limit:
-            counter += 1
 
             item = pywikibot.ItemPage(repo, artist_id)
-            print("Adding P136 claim to "+artist_id)
-            if 'P136' not in item.claims:
+            hasGenre = 'P136' in item.claims
+            print(" "+artist_id+" already has claim for P136\n") if hasGenre
+            hasPublicArtGenre = pywikibot.ItemPage(repo, 'Q42030') in [claim.target for claim in item.claims['P242']]
+            print(" "+artist_id+" already has Q42030 as target of P136 claim\n") if hasPublicArtGenre
+
+            if not hasGenre or not hasPublicArtGenre
+                print("Adding P136 claim to "+artist_id)
                 claim = pywikibot.Claim(repo, 'P136') #Adding artistic genre of oeuvre (P136)
                 target = pywikibot.ItemPage(repo, 'Q557141') #Connecting P136 with 'Public art' (Q557141)
                 claim.setTarget(target)
@@ -27,7 +32,9 @@ with open('wikidata_ids.json', 'r') as file_contents:
                 url = pywikibot.Claim(repo, 'P854') #P854, reference URL
                 url.setTarget("https://picasso.iro.umontreal.ca/~mona/api/v3/artists")
                 claim.addSource(url, summary='Bot: Adding sources to artistic genre of oeuvre (P136).')
+
+                counter += 1
             else:
-                print(" "+artist_id+" already has claim for P136\n")
+                print("Skipping...")
         else:
             print("Did not modify "+artist_id+"; hit modification limit.")
